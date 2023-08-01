f6a4997288221a76de9fb7aefad729136330eb86
JuanM <58197473+juanmggb@users.noreply.github.com>
2023-07-31 20:13:35 -0600

    JM added stuff

--- store/models.py
+++ store/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+from django.urls import reverse
+
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250, db_index=True)
@@ -12,6 +15,9 @@ class Category(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("list-category", args=[self.slug])
 
 
 class Product(models.Model):
@@ -36,3 +42,6 @@ class Product(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+    def get_absolute_url(self):
+        return reverse("product-info", args=[self.slug])
